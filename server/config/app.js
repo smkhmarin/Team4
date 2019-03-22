668db620fa2a3e9f8ec33c7609d698bc5b33b9a4
Janice <janiceng@ufl.edu>
2019-03-22 15:39:40 -0400

    merge conflicts fixes

--- server/config/app.js
+++ server/config/app.js
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-require('dotenv').config();
-
-=======
->>>>>>> janice
 var config = require('./config'), 
     mongoose = require('mongoose'),   
     express = require('./express');
 
 module.exports.start = function() {
   var app = express.init();
-<<<<<<< HEAD
-  console.log('tespt');
-  app.listen(config.port, function() {
-    console.log('test');
-    console.log('App listening on port', config.port);
-  });
-};
-=======
   app.listen(config.port, function() {
     console.log('App listening on port', config.port);
   });
 };
->>>>>>> janice
