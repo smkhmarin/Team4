a14d5c811875a517e479966609c0ffada6e345d4
goode bye <bye.goode@gmail.com>
2019-04-16 15:16:00 -0400

    removes extraneous endline spaces

--- server/models/user.server.model.js
+++ server/models/user.server.model.js
@@ -31,17 +31,17 @@ const userSchema = new Schema({
 
 userSchema.methods.setPassword = function(password) {
 	//create a unique salt for each user
-	this.passwordSalt = crypto.randomBytes(16).toString('hex'); 
-	
+	this.passwordSalt = crypto.randomBytes(16).toString('hex');
+
 	//hash the salt and password
-    this.passwordHash = crypto.pbkdf2Sync(password, this.passwordSalt, 1000, 64, `sha512`).toString(`hex`); 
+    this.passwordHash = crypto.pbkdf2Sync(password, this.passwordSalt, 1000, 64, `sha512`).toString(`hex`);
 };
 
-userSchema.methods.validPassword = function(password) { 
-    var passwordHash = crypto.pbkdf2Sync(password, this.passwordSalt, 1000, 64, `sha512`).toString(`hex`); 
+userSchema.methods.validPassword = function(password) {
+    var passwordHash = crypto.pbkdf2Sync(password, this.passwordSalt, 1000, 64, `sha512`).toString(`hex`);
     console.log(this.passwordHash === passwordHash);
-    return this.passwordHash === passwordHash; 
-}; 
+    return this.passwordHash === passwordHash;
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
